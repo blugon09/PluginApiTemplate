81054771f929067cfc73258a09c05a05f44788b3
Blugon <71944969+Blugon0921@users.noreply.github.com>
2021-11-09 10:01:51 +0900

    build.gradle.kts Fix

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # PluginApiTemplate
 
-[![Kotlin](https://img.shields.io/badge/Java-16.0.2-FF7700.svg?logo=java)]()
-[![Kotlin](https://img.shields.io/badge/Kotlin-1.5.30-186FCC.svg?logo=kotlin)]()
+[![PluginApiTemplate](https://img.shields.io/badge/PluginApiTemplate-1.0.0_SNAPSHOT-blue.svg)]()
+<br><br>
+[![Kotlin](https://img.shields.io/badge/Java-16-FF7700.svg?logo=java)]()
+[![Kotlin](https://img.shields.io/badge/Kotlin-1.5.31-186FCC.svg?logo=kotlin)]()
 [![PaperMC](https://img.shields.io/badge/PaperMC-1.17-222222.svg)]()
 
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     kotlin("jvm") version "1.5.31"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("org.jetbrains.dokka") version "1.5.0"
     `maven-publish`
 }
 
 group = "io.github.blugon09"
-version = "0.0.1"
-
+version = "1.0.0-SNAPSHOT"
 
 
 repositories {
@@ -16,24 +16,71 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
-
-    implementation("net.kyori:adventure-api:4.9.2")
+    implementation("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    implementation("net.kyori:adventure-api:4.9.3")
 }
-
 
 tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+
     create<Jar>("sourcesJar") {
-        archiveClassifier.set("source")
-        sourceSets["main"].allSource
+        archiveClassifier.set("sources")
+        from(sourceSets["main"].allSource)
+    }
+
+    create<Jar>("javadocJar") {
+        archiveClassifier.set("javadoc")
+        dependsOn("dokkaHtml")
+        from("$buildDir/dokka/html")
     }
 }
+
 
 publishing {
     publications {
         create<MavenPublication>(rootProject.name) {
-            artifact(tasks["sourcesJar"])
             from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            repositories {
+                maven {
+                    val releasesRepoUrl = "https://repo.projecttl.net/repository/maven-releases/"
+                    val snapshotsRepoUrl = "https://repo.projecttl.net/repository/maven-snapshots/"
+                    url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+
+                    credentials.runCatching {
+                        username = project.properties["username"] as String?
+                        password = project.properties["password"] as String?
+                    }
+                }
+
+                pom {
+                    name.set(rootProject.name)
+                    description.set("")
+                    url.set("https://github.com/blugon09/${rootProject.name}")
+//                    licenses {
+//                        license {
+//                            name.set("")
+//                            url.set("")
+//                        }
+//                    }
+                    developers {
+                        developer {
+                            id.set("blugon09")
+                            name.set("Blugon")
+                            email.set("blugon0921@blugon.kro.kr")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/blugon09/${rootProject.name}.git")
+                        developerConnection.set("scm:git:https://github.com/blugon09/${rootProject.name}.git")
+                        url.set("https://github.com/blugon09/${rootProject.name}.git")
+                    }
+                }
+            }
         }
     }
 }
